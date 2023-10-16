896092d51680b4a879773c21e025557405f0c7c4
Dai Ngoc <tangocdai13@gmail.com>
2023-10-16 15:36:52 +0700

    Build functions for Session

--- includes/session.php
+++ includes/session.php
@@ -1,2 +1,52 @@
 <?php
 if (!defined('_INCODE')) die('access denied ...');
+
+function setSession($key, $value) {
+    if (!empty(session_id())) {
+        $_SESSION[$key] = $value;
+        return true;
+    }
+
+    return false;
+}
+
+function getSession($key = '') {
+    if (empty($key)) {
+        return $_SESSION;
+    } else {
+        if (isset($_SESSION[$key])) {
+            return $_SESSION[$key];
+        }
+    }
+
+    return false;
+}
+
+function removeSession($key = '') {
+    if (empty($key)) {
+        session_destroy();
+        return true;
+    } else {
+        if (isset($_SESSION[$key])) {
+            unset($_SESSION[$key]);
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function setFlashSession($key, $value) {
+    $key = 'flash_'.$key;
+
+    setSession($key, $value);
+}
+
+function getFlashSession($key) {
+    $key = 'flash_'.$key;
+
+    if ($data = getSession($key)) {
+        removeSession($key);
+        return $data;
+    }
+}
--- index.php
+++ index.php
@@ -5,6 +5,7 @@ require_once 'config.php';
 require_once 'includes/function.php';
 require_once 'includes/connect.php';
 require_once 'includes/database.php';
+require_once 'includes/session.php';
 
 $modules = _MODULES_DEFAULT;
 $action = _ACTION_DEFAULT;
